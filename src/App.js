436e892f38dbefa855f48a808548f17cdb93fdf8
kalwalt <github@kalwaltart.it>
2019-05-09 01:10:20 +0200

    this work!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
-    <a-scene embedded arjs>
-      <a-marker preset="hiro">
-          <a-box position='0 0.5 0' material='color: yellow;'></a-box>
-      </a-marker>
-      <a-entity camera></a-entity>
-    </a-scene>
+    <div></div>
   );
 }
 
